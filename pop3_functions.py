b10afe7591e8d4db09c0dec05c057001043189c4
ducnhat24 <127527389+ducnhat24@users.noreply.github.com>
2023-12-04 20:32:08 +0700

    Update pop3_functions.py

--- pop3_functions.py
+++ pop3_functions.py
@@ -1,60 +1,57 @@
 import socket
 
-
-def smtp_valid_reponse (code): 
+def pop3_valid_reponse(code) :
     code = code[:3]
-    code = int(code)
-    if (200 <= code < 400) : return 1
-    else : 
-        print("Error occurred. Quitting...")
+    if (code == "+OK") : return 1
+    else :
+        print("Error occurred. Quiting...")
         return 0
     
-
-def send_mail(SMTP_HOST, SMTP_PORT, BUFFER_SIZE, DOMAIN, smtp_socket, username):
+def receive_mail(POP3_HOST, POP3_PORT, BUFFER_SIZE, DOMAIN, pop3_socket, username, password):
     # whenever u want to sent your mail using SMTP, u have to send these following commands:
-    # HELO / EHLO
-    # MAIL FROM - includes a sender mailbox
-    # RCPT TO - includes a destination mailbox
-    # DATA - mail data
-    # QUIT
-    # After each command, u have to check the Code respone by the server (using valid_response func)
-    print("Enter destination mail: ")
-    destination_user = input()
-    print("Enter mail content: ")
-    data = input()
-
-    smtp_socket.connect((SMTP_HOST, SMTP_PORT))
-    if (smtp_valid_reponse(smtp_socket.recv(BUFFER_SIZE).decode()) == 0) : 
-        smtp_socket.sendall(('QUIT\r\n').encode())
+    # USER
+    # PASS
+    # STAT
+    # LIST
+    # RETR
+    print("Opening the first unread message:")
+    pop3_socket.connect((POP3_HOST, POP3_PORT))
+    if (pop3_valid_reponse(pop3_socket.recv(BUFFER_SIZE).decode()) == 0) : 
+        pop3_socket.sendall(('QUIT\r\n').encode())
+        return -1
+    
+    pop3_socket.sendall(('USER ' + username + DOMAIN + '\r\n').encode())
+    if (pop3_valid_reponse(pop3_socket.recv(BUFFER_SIZE).decode()) == 0) : 
+        pop3_socket.sendall(('QUIT\r\n').encode())
+        return -1
+    
+    pop3_socket.sendall(('PASS ' + password + '\r\n').encode())
+    if (pop3_valid_reponse(pop3_socket.recv(BUFFER_SIZE).decode()) == 0) : 
+        pop3_socket.sendall(('QUIT\r\n').encode())
+        return -1
+    
+    pop3_socket.sendall(('STAT' + '\r\n').encode())
+    if (pop3_valid_reponse(pop3_socket.recv(BUFFER_SIZE).decode()) == 0) : 
+        pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
 
-    # HELO/EHLO
-    smtp_socket.sendall(('EHLO testserver.com\r\n' ).encode())
-    if (smtp_valid_reponse(smtp_socket.recv(BUFFER_SIZE).decode()) == 0) : 
-        smtp_socket.sendall(('QUIT\r\n').encode())
-        return -1    
-    # MAIL FROM
-    smtp_socket.sendall(('MAIL FROM: ' + username + DOMAIN + '\r\n').encode())
-    if (smtp_valid_reponse(smtp_socket.recv(BUFFER_SIZE).decode()) == 0) : 
-        smtp_socket.sendall(('QUIT\r\n').encode())
-        return -1
-    # RCPT TO
-    smtp_socket.sendall(('RCPT TO: ' + destination_user + DOMAIN + '\r\n').encode())
-    if (smtp_valid_reponse(smtp_socket.recv(BUFFER_SIZE).decode()) == 0) : 
-        smtp_socket.sendall(('QUIT\r\n').encode())
+    pop3_socket.sendall(('LIST' + '\r\n').encode())
+    if (pop3_valid_reponse(pop3_socket.recv(BUFFER_SIZE).decode()) == 0) : 
+        pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
 
-    # DATA
-    smtp_socket.sendall(('DATA\r\n').encode())
-    if (smtp_valid_reponse(smtp_socket.recv(BUFFER_SIZE).decode()) == 0) : 
-        smtp_socket.sendall(('QUIT\r\n').encode())
+    # Return the content of the first mail
+    pop3_socket.sendall(b'RETR 1\r\n')
+    message = pop3_socket.recv(BUFFER_SIZE).decode()
+    if (pop3_valid_reponse(message) == 0) : 
+        pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
-    smtp_socket.sendall((data + '\r\n').encode())
-    smtp_socket.sendall(('.\r\n').encode())
-    if (smtp_valid_reponse(smtp_socket.recv(BUFFER_SIZE).decode()) == 0) : 
-        smtp_socket.sendall(('QUIT\r\n').encode())
+    print(message)
+    pop3_socket.sendall(b'DELE 1\r\n')
+    message = pop3_socket.recv(BUFFER_SIZE).decode()
+    if (pop3_valid_reponse(message) == 0) : 
+        pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
-
-    smtp_socket.sendall(('QUIT\r\n').encode())
+    pop3_socket.sendall(('QUIT\r\n').encode())
     print("Press Enter to continue.")
     input()
