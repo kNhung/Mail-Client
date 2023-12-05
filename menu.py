612aa2657ee67b5a6839f17c991ab61d0ee6d845
ducnhat24 <127527389+ducnhat24@users.noreply.github.com>
2023-12-05 09:19:01 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -6,24 +6,26 @@ from consolemenu import *
 from consolemenu.items import *
 
 def change_username():
-    global username
-    print("Please enter your username:")
-    username = input()
+    client.USERNAME = input("Please enter your username:")
+    print(client.USERNAME)
+    print("Press Enter to continue.")
+    input()
+    menu()
 
+def show_current_user(): 
+    print(client.USERNAME)
+    print("Press Enter to continue.")
+    input()
 
 def menu():
-    # Initialize socket:
-    smtp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    pop3_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    username = "dn24"
-    password = "xxxx"
     # Create the menu
     menu = ConsoleMenu("SIMPLE MAIL CLIENT", "Group 12!")
     change_user_option = FunctionItem("Choose Username", change_username)
-    send_option = FunctionItem("Send Mail Using SMTP", lambda:smtp_functions.send_mail(client.SMTP_HOST, client.SMTP_PORT, client.BUFFER_SIZE, client.DOMAIN, smtp_socket, username))
-    receive_option = FunctionItem("Receive Mail Using Pop3", lambda:pop3_functions.receive_mail(client.POP3_HOST, client.POP3_PORT, client.BUFFER_SIZE, client.DOMAIN, pop3_socket, username, password))
-
+    send_option = FunctionItem("Send Mail Using SMTP", smtp_functions.send_mail)
+    receive_option = FunctionItem("Receive Mail Using Pop3", pop3_functions.receive_mail)
+    test_option = FunctionItem("Show current user", show_current_user)
     menu.append_item(change_user_option)
     menu.append_item(send_option)
     menu.append_item(receive_option)
+    menu.append_item(test_option)
     menu.show()
