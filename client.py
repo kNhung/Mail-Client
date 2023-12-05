fdf37635c7f31047ece77d211bded7b4f9e87e1e
ducnhat24 <127527389+ducnhat24@users.noreply.github.com>
2023-12-05 09:17:59 +0700

    Update client.py

--- client.py
+++ client.py
@@ -16,9 +16,9 @@ SMTP_HOST = config['Server']['SMTPServer']
 SMTP_PORT = config['Server']['SMTP']
 POP3_HOST = config['Server']['Pop3Server']
 POP3_PORT = config['Server']['POP3']
-DOMAIN = config['Server']['domain']
-# USERNAME = config['Authentication']['username']
-# PASSWORD = config['Authentication']['password']
+USERNAME = config['Authentication']['username']
+PASSWORD = config['Authentication']['password']
+
 
 if __name__ == "__main__":
     menu()
