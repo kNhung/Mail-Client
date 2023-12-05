958bfb59b8b366865c05eb54ef4e597daa128c7e
ducnhat24 <127527389+ducnhat24@users.noreply.github.com>
2023-12-05 09:19:41 +0700

    Update pop3_functions.py

--- pop3_functions.py
+++ pop3_functions.py
@@ -1,54 +1,58 @@
 import socket
-
+import client
 def pop3_valid_reponse(code) :
     code = code[:3]
     if (code == "+OK") : return 1
     else :
         print("Error occurred. Quiting...")
+        print("Press Enter to continue.")
+        input()
         return 0
     
-def receive_mail(POP3_HOST, POP3_PORT, BUFFER_SIZE, DOMAIN, pop3_socket, username, password):
+def receive_mail():
     # whenever u want to sent your mail using SMTP, u have to send these following commands:
     # USER
     # PASS
     # STAT
     # LIST
     # RETR
+    pop3_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     print("Opening the first unread message:")
-    pop3_socket.connect((POP3_HOST, POP3_PORT))
-    if (pop3_valid_reponse(pop3_socket.recv(BUFFER_SIZE).decode()) == 0) : 
+    pop3_socket.connect((client.POP3_HOST, client.POP3_PORT))
+    if (pop3_valid_reponse(pop3_socket.recv(client.BUFFER_SIZE).decode()) == 0) : 
         pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
     
-    pop3_socket.sendall(('USER ' + username + DOMAIN + '\r\n').encode())
-    if (pop3_valid_reponse(pop3_socket.recv(BUFFER_SIZE).decode()) == 0) : 
+    pop3_socket.sendall(('USER ' + client.USERNAME + '\r\n').encode())
+    if (pop3_valid_reponse(pop3_socket.recv(client.BUFFER_SIZE).decode()) == 0) : 
         pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
     
-    pop3_socket.sendall(('PASS ' + password + '\r\n').encode())
-    if (pop3_valid_reponse(pop3_socket.recv(BUFFER_SIZE).decode()) == 0) : 
+    pop3_socket.sendall(('PASS ' + client.PASSWORD + '\r\n').encode())
+    if (pop3_valid_reponse(pop3_socket.recv(client.BUFFER_SIZE).decode()) == 0) : 
         pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
     
     pop3_socket.sendall(('STAT' + '\r\n').encode())
-    if (pop3_valid_reponse(pop3_socket.recv(BUFFER_SIZE).decode()) == 0) : 
+    if (pop3_valid_reponse(pop3_socket.recv(client.BUFFER_SIZE).decode()) == 0) : 
         pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
 
     pop3_socket.sendall(('LIST' + '\r\n').encode())
-    if (pop3_valid_reponse(pop3_socket.recv(BUFFER_SIZE).decode()) == 0) : 
+    if (pop3_valid_reponse(pop3_socket.recv(client.BUFFER_SIZE).decode()) == 0) : 
         pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
 
     # Return the content of the first mail
     pop3_socket.sendall(b'RETR 1\r\n')
-    message = pop3_socket.recv(BUFFER_SIZE).decode()
+    message = pop3_socket.recv(client.BUFFER_SIZE).decode()
     if (pop3_valid_reponse(message) == 0) : 
         pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
     print(message)
     pop3_socket.sendall(b'DELE 1\r\n')
-    message = pop3_socket.recv(BUFFER_SIZE).decode()
+    message = pop3_socket.recv(client.BUFFER_SIZE).decode()
     if (pop3_valid_reponse(message) == 0) : 
         pop3_socket.sendall(('QUIT\r\n').encode())
         return -1
